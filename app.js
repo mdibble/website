24950c8c556547c7124752cec323a7c4afa35bc9
Matthew Dibble <mdibble@rogers.com>
2020-02-16 19:04:14 -0500

    Improved mobile compatibility

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const navSlide = () => {
 
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
 
     burger.addEventListener('click', ()=> {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+
+    nav.addEventListener('click', ()=> {
         nav.classList.toggle('nav-active');
         burger.classList.toggle('toggle');
     });
